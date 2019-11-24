e057eb3df676f3fbe971ef5a501e737afed48ada
notreniev <everton.canez@gmail.com>
2019-11-24 14:04:43 -0300

    first commit

---
+++ app/src/main/java/utils/OnDetectScrollListener.java
@@ -0,0 +1,15 @@
+package utils;
+
+/**
+ * Created by canez on 13/10/14.
+ */
+public interface OnDetectScrollListener {
+
+    void onUpScrolling();
+
+    void onDownScrolling();
+
+    void onBottomScrolled();
+
+    void onTopScrolled();
+}
